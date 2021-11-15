82a4879ddccdb2212ae9c4a07223c0a10b75976b
raposojoaopaulo <raposojoaopaulo@gmail.com>
2021-11-15 11:25:17 -0300

    Aula 51

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :coins
+  resources :home
+
+  root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
